464b4c4e3419d957e7093be7bfebd3c84d4a2e29
frostb1ten <68353531+frostb1ten@users.noreply.github.com>
2022-03-11 22:42:11 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
 use error_chain::error_chain;
-use std::fs::File;
 use std::fs;
 use std::io::Write;
-use std::io::{self, BufRead};
-use std::path::Path;
-extern crate url;
+use std::io::{BufRead};
+use std::time::Duration;
+
 
 error_chain! {
     foreign_links {
@@ -13,82 +14,130 @@ error_chain! {
     }
 }
 
-
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Starting!");
-    if let Ok(lines) = read_lines("./hosts.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let client = reqwest::Client::builder()
-                    .danger_accept_invalid_certs(true)
-                    .build()?;
-                let website = ip.replace("Bugbounty","RustScan\"Bugbounty");
-                let res = client
-                    .get(website.clone())
-                    .send()
-                    .await;
-                let res = match res {
-                    Ok(v) => v,
-                    Err(_err) => continue,
-                };
-                if res.status() == 200 {
-                    let body = res.text().await?;
-                    if body.contains("RustScan\"bugbounty") {
-                        let mut file = fs::OpenOptions::new()
-                            .write(true)
-                            .append(true)
-                            .create(true)
-                            .open("output.txt")
-                            .unwrap();
-                        write!(file, "{}", website)?;
-                        println!("XSS likely in (double quote) {}", website);
-                    }
-                    let website2 = ip.replace("Bugbounty","RustScan\'Bugbounty");
-                    let _res = client
-                        .get(website2.clone())
-                        .send()
-                        .await?;
-                    if body.contains("RustScan\'bugbounty") {
-                        let mut file = fs::OpenOptions::new()
-                            .write(true)
-                            .append(true)
-                            .create(true)
-                            .open("XSSSingle.txt")
-                            .unwrap();
-                        write!(file, "{}", website2)?;
-                        println!("XSS likely in (Single quote) {}", website);
-                    }
-                    if body.contains("ORA-") {
-                        let mut file = fs::OpenOptions::new()
-                            .write(true)
-                            .append(true)
-                            .create(true)
-                            .open("OraOutput.txt")
-                            .unwrap();
-                        write!(file, "{}", website)?;
-                        println!("SQLi (Oracle) likely in {}", website)
-                        }
-                    if body.contains("MySQL") {
-                        let mut file = fs::OpenOptions::new()
-                            .write(true)
-                            .append(true)
-                            .create(true)
-                            .open("MysqlOutput.txt")
-                            .unwrap();
-                        write!(file, "{}", website)?;
-                        println!("Found the words MySQL in {}\r\n", website)
-                        }
-                    }
+    let client = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+    for line in std::fs::read("./hosts.txt").expect("Could not read file").lines() {
+        if let Ok(ip) = line {
+            let website = ip.replace("Bugbounty","RustScan\"Bugbounty");
+            println!("{}", website);
+            let res = client
+                .get(&website)
+                .timeout(Duration::from_secs(5))
+                .send();
+            let res = match res.await {
+                Ok(v) => v,
+                Err(_) => {
+                    continue
+                },
+            };
+            if res.status() == 200 {
+                let body = res.text().await?;
+                let mut file = fs::OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .create(true)
+                    .open("./analysis/live_sites.txt")
+                    .unwrap();
+                write!(file, "{}\n", website)?;
+                if body.contains("RustScan\"Bugbounty") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/XSS.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                    println!("XSS likely in (double quote) {}", website);
                 }
+                if website.contains("redirect") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/redirects.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if website.contains("WFSServer") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/WFSServer.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if website.contains(".pl") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/Perl.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if website.contains(".cgi") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/cgi.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if body.contains("Error") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/error.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if body.contains("<?php") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/phpsource.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if website.contains("-bin") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/bins.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                }
+                if body.contains("ORA-") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/OraOutput.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                    println!("SQLi (Oracle) likely in {}", website)
+                }
+                if body.contains("MySQL") {
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .create(true)
+                        .open("./analysis/MysqlOutput.txt")
+                        .unwrap();
+                    write!(file, "{}\n", website)?;
+                    println!("Found the words MySQL in {}\r\n", website)
+                }
+            }
         }
     }
     Ok(())
 }
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-
